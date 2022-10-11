5c6e507a5f625a1aaa0663dc67d6010a329bdca3
P4wlos222 <pawel.czyz@woli.edu.pl>
2022-10-11 21:25:11 +0200

    Second

--- server.js
+++ server.js
@@ -1,6 +1,52 @@
-import { createServer } from 'http';
+const fs = require('fs');
+const session = require('express-session');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(express.json());
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    fs.readFile("index.html", function(err, data){
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.get('/login', function (req, res) {
+    fs.readFile("login.html", function(err, data){
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+
+app.post('/auth', function (req, res) {
+    console.log(req.body.user,req.body.passwd);
+    return res.redirect("/")
+});
+
+var server = app.listen(process.env.PORT, function () {
+    console.log('Node server is running..');
+});
