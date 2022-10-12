de7f018428f72639cbe3182dba3b9b38481080c3
P4wlos222 <pawel.czyz@woli.edu.pl>
2022-10-12 23:46:26 +0200

    ...

--- register.js
+++ register.js
@@ -2,11 +2,6 @@ const db = require(__dirname + "/dbconnect.js");
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
-/*const passwd = 'miaurek2137';
-bcrypt.hash(passwd, saltRounds, function(err, hash) {
-    console.log(hash);
-});*/
-
 
 function DbAppend(data){
     let salt = bcrypt.genSaltSync(saltRounds, function(err,hash){
@@ -15,13 +10,7 @@ function DbAppend(data){
     let hashedPassword = bcrypt.hashSync(data.Password, salt, function(err,hash){
         if (err) throw err;
     });
-    //console.log(data);
-    //console.log(typeof data.Password);
-    //var plain = data.Password
-    //console.log(typeof plain, plain);
-    //bcrypt.hash(plain, saltRounds, function(err, hash) {
-    //    console.log(hash);
-    //});
+
     console.log(typeof hashedPassword, hashedPassword);
     db.query('INSERT INTO Users(UUID,Email,Phone,Password,FirstName,LastName,DisplayName) VALUES (UUID_TO_BIN(UUID()),?,?,?,?,?,?)',
     [data.Email,data.Phone,hashedPassword,data.FirstName,data.LastName,data.DisplayName],
