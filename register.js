af82d5663f744e9c464f7b9cff17c57408ad1603
P4wlos222 <pawel.czyz@woli.edu.pl>
2022-10-12 21:37:19 +0200

    Update register.js

--- register.js
+++ register.js
@@ -4,10 +4,10 @@ const saltRounds = 10;
 
 function DbAppend(data){
     console.log(data);
-    console.log(data.Password);
+    console.log(typeof data.Password);
     hashedPassword = bcrypt.hash(data.Password, saltRounds, function(err, hash) {
         console.log(hash);
-        return hash;
+        return hash 
     });
     console.log(hashedPassword);
     db.query('INSERT INTO Users(UUID,Email,Phone,Password,FirstName,LastName,DisplayName) VALUES (UUID_TO_BIN(UUID()),?,?,?,?,?,?)',
