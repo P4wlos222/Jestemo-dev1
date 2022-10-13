615a7afdc25f35a4976717e86f8429fda9bff0b2
P4wlos222 <pawel.czyz@woli.edu.pl>
2022-10-13 21:24:31 +0200

    ...

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,3 +1,22 @@
 const responseField = document.querySelector('#response')
+const email = document.querySelector('#email')
+const password = document.querySelector('#passwd')
+const form = document.querySelector('#logform')
 
-responseField.innerHTML = "Que???"
+form.addEventListener('submit', (e) =>{
+    let messages = []
+    if (email.value === '' || email.value == null)
+    {
+        messages.push('Wpisz adres e-mail!')
+    }
+    if (password.value === '' || password.value == null)
+    {
+        messages.push('Wpisz hasło!')
+    }
+
+    if (messages.length > 0)
+    {
+        e.preventDefault()
+        responseField.innerHTML = messages.join(', ')
+    }
+})
